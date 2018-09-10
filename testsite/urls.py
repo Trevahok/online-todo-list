c2eef0a4d33afee95f68dff2fcdd8efb439390bb
Trevahok <vighneshss@gmail.com>
2018-09-10 19:14:17 +0530

    add static routing

--- testsite/urls.py
+++ testsite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from testsite import settings 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('todolist.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
