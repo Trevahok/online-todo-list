a76b69a89f44c6faeb78a7f31f832b394c23ce7c
Trevahok <vighneshss@gmail.com>
2018-09-10 20:00:25 +0530

    nope .. there goes my self esteem

--- testsite/settings.py
+++ testsite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0!6#k2i9c(5nhb-@99w^xjbizgoc&3rz58t+iz)w&v93uvoflg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1' , '.herokuapp.com']
 
