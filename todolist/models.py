18ac0b253b75b313cca525ca0c9bc651eefd8780
darkkknight <siddarthsairaj@gmail.com>
2018-09-11 21:07:59 +0530

    edit stuff lmao  committed:

--- todolist/models.py
+++ todolist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class List(models.Model):
-    item = models.CharField(max_length=255)
+    item = models.CharField(max_length=1000)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
--- todolist/views.py
+++ todolist/views.py
@@ -8,11 +8,11 @@ def home(request):
 
 		if form.is_valid():
 			form.save()
-			all_items = List.objects.all()
+			all_items = List.objects.all
 			messages.success(request,('Task Has Been Added To List yayy!'))
 			return render(request, 'home.html',{'all_items':all_items})
 	else:
-		all_items = List.objects.all()
+		all_items = List.objects.all		
 		return render(request,'home.html',{'all_items':all_items})
 def delete(request, list_id):
 	item = List.objects.get(pk=list_id)
@@ -29,4 +29,17 @@ def uncross(request, list_id):
 	item.completed= False
 	item.save()
 	return redirect('home')
+def edit(request,list_id):
+	if request.method == 'POST':
+		item = List.objects.get(pk=list_id)
+		item.delete()
+		form = ListForm(request.POST or None)
 
+		if form.is_valid():
+			form.save()
+			all_items = List.objects.all
+			messages.success(request,('Task Has Been updated yayy!'))
+			return redirect('home')
+	else:
+		item = List.objects.get(pk=list_id)	
+		return render(request,'edit.html',{'items':item})
